705b001e1063db580ada32c9b4d9a6404f30f288
chaerin2739 <118095564+chaerin2739@users.noreply.github.com>
2024-01-15 16:26:41 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function check(mood, goodCallback, badCallback) {
+  if (mood === "good") {
+    goodCallback();
+  } else {
+    badCallback();
+  }
+}
+function dance() {
+  console.log("dance");
+}
+function sing() {
+  console.log("sing");
+}
+function cry() {
+  console.log("cry");
+}
+check("good", dance, cry);
