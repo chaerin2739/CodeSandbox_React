a8c47324e77fa2e7ddd205f62fd28cf82c791536
YOO CHAERIN <chlin2739@naver.com>
2024-01-22 17:22:52 +0900

    객체 불러오기

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-for (let i = 0; i < 100; i++) {
-    console.log("유채린");
+let person = {
+    name: "유채린",
+    age: 23,
+    tall: 158
+  };
+  
+  const personkey = Object.keys(person);
+  for (let i = 0; i < personkey.length; i++) {
+    const curKey = personkey[i];
+    const curvalue = person[curKey];
+  
+    console.log(`${curKey} : ${curvalue}`);
   }
   
